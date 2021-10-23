412986bbecdc10b17f88a468b8c98d3a76531c98
Oiks-Cage <mrsolo322@inbox.ru>
2021-10-23 19:13:14 +0500

    Добавление Wallet_and_SampleToken

---
+++ Wallet_and_SampleToken/SampleToken.sol
@@ -0,0 +1,52 @@
+pragma ton-solidity >= 0.35.0;
+pragma AbiHeader expire;
+
+contract SampleToken {
+   
+    struct animal {
+        string name;
+        uint maxAge;
+        string class;
+        bool inRebBook;    
+    }
+
+    animal[] animalsArr;
+    mapping (uint => uint) animalAndOwner;
+    mapping (uint => uint) animalsPrice;
+
+    modifier chekAnimalsOwner (uint animalId) {
+        require(animalAndOwner[animalId] == msg.pubkey());
+        tvm.accept();
+        _;
+    }
+
+    modifier chekAnimalsName (string name) {
+        for (uint256 i = 0; i < animalsArr.length; i++) {
+            require(animalsArr[i].name != name);
+        }
+        tvm.accept();
+        _;
+    }
+
+    function createAnimals (string name, uint maxAge, string class, bool inRedBook) public chekAnimalsName(name) {
+        animalsArr.push(animal(name, maxAge, class, inRedBook));
+        uint lastNome = animalsArr.length -1;
+        animalAndOwner[lastNome] = msg.pubkey();
+    }
+
+    function createAnimalPrice (uint animalId, uint animalPrice) public chekAnimalsOwner(animalId){
+        animalsPrice[animalId] = animalPrice;
+    }
+
+    function getAnimalPrice (uint animalId) public returns (uint price){
+        price = animalsPrice[animalId];
+    }
+
+    constructor() public {
+        require(tvm.pubkey() != 0, 101);
+        require(msg.pubkey() == tvm.pubkey(), 102);
+        tvm.accept();
+
+    }
+
+}
---
+++ Wallet_and_SampleToken/Wallet.sol
@@ -0,0 +1,29 @@
+pragma ton-solidity >= 0.35.0;
+pragma AbiHeader expire;
+
+contract Wallet {
+
+    constructor() public {
+        require(tvm.pubkey() != 0, 101);
+        require(msg.pubkey() == tvm.pubkey(), 102);
+        tvm.accept();
+    }
+
+    modifier checkOwnerAndAccept {
+        require(msg.pubkey() == tvm.pubkey(), 100);
+		tvm.accept();
+		_;
+	}
+
+    function sendTransactionMyCommission(address dest, uint128 value) public pure checkOwnerAndAccept {
+        dest.transfer(value, true, 1);
+    }
+
+    function sendTransactionYourCommission(address dest, uint128 value) public pure checkOwnerAndAccept {
+        dest.transfer(value, true, 0);
+    }
+
+    function sendTransactionAllMoneyAndDeletingWallet(address dest) public pure checkOwnerAndAccept {
+        dest.transfer(1, true, 160);
+    }
+}
